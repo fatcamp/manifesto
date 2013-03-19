cafbc4f28ef9d190c9c22a6c05d21214d7e6e655
Melissa <melissa.liu@outlook.com>
2013-03-19 17:08:32 -0400

    Fixed searchform

--- searchform.php
+++ searchform.php
@@ -1,7 +1,5 @@
-<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
-<p>
-<input type="submit" id="searchsubmit" value="" />
-<label for="s">SEARCH</label>
-<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" />
-</p>
+<form action="<?php echo esc_url( home_url( '/' ) ); ?>" id="searchform" method="get">
+	<label for="s">Search</label>
+    <input type="text" id="s" name="s" placeholder="<?php the_search_query(); ?>" />
+    <input type="submit" value="Search" id="searchsubmit" />
 </form>
